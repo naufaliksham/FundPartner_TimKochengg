4729bc4eb17f929b107fd05e5149631fab533c34
petersonhz25 <82883040+zehamless@users.noreply.github.com>
2023-05-21 20:22:51 +0700

    fix import

--- app/Models/Usaha.php
+++ app/Models/Usaha.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Usaha extends Model
 {
