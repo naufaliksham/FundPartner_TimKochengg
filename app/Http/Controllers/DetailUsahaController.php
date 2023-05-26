bd9a86d508d96ededccf72d33ef0ae6fb3fa8f8b
petersonhz25 <82883040+zehamless@users.noreply.github.com>
2023-05-26 09:53:43 +0700

    fix detailUsaha route

--- app/Http/Controllers/DetailUsahaController.php
+++ app/Http/Controllers/DetailUsahaController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Usaha;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class DetailUsahaController extends Controller
 {
     
-    public function showDetailUsaha($request)
+    public function showDetailUsaha()
     {
-        $details = Usaha::with('usaha')->where('id_mitra', $request)->get();
+        $userID = Auth::id();
+        $details = Usaha::with('usaha')->where('id_mitra', $userID)->get();
         return view('usaha.detailUsaha', compact('details'));
     }
 }
