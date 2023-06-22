87730f484b800e509fc0629192b73172b8d14862
petersonhz25 <82883040+zehamless@users.noreply.github.com>
2023-06-22 13:25:41 +0700

    fix footer

--- resources/views/layouts_investor/footer.blade.php
+++ resources/views/layouts_investor/footer.blade.php
@@ -1,98 +1,47 @@
         <!--Site Footer Start-->
         <footer class="site-footer">
             <div class="container">
-                <div class="row">
+                <div class="row" style="justify-content: space-around;">
                     <div class="col-xl-4 col-lg-6 col-md-6">
                         <div class="footer-widget__column footer-widget__about wow fadeInUp animated"
                             data-wow-delay="100ms"
-                            style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp;">
+                            style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp; display: flex; justify-content: center; align-items: center; flex-direction: column;">
                             <div class="footer-widget__title">
-                                <h3>About</h3>
+                                <h3>About Us</h3>
                             </div>
                             <div class="footer-widget_about_text">
-                                <p>Id quas utamur delicata qui, vix ei prima mentitum omnesque. Duo corrumpit cotidieque
-                                    ne.</p>
+                                <p>Platform kami memberikan manfaat kepada anda yang ingin mengembangkan usaha yang anda miliki dengan pelayanan dan kualitas terbaik</p>
                             </div>
-                            <form>
-                                <div class="footer_input-box">
-                                    <input type="Email" placeholder="Email Address">
-                                    <button type="submit" class="button"><i class="fa fa-envelope"></i></button>
-                                </div>
-                            </form>
                         </div>
                     </div>
-                    <div class="col-xl-2 col-lg-3 col-md-3">
-                        <div class="footer-widget__column footer-widget__company wow fadeInUp animated"
+                    <div class="col-xl-4 col-lg-6 col-md-6">
+                        <div class="footer-widget__column footer-widget__contact wow fadeInUp animated"
                             data-wow-delay="200ms"
-                            style="visibility: visible; animation-delay: 200ms; animation-name: fadeInUp;">
+                            style="visibility: visible; animation-delay: 200ms; animation-name: fadeInUp; display: flex; justify-content: center; align-items: center; flex-direction: column;">
                             <div class="footer-widget__title">
-                                <h3>Company</h3>
+                                <h3>Hubungi Kami</h3>
                             </div>
-                            <ul class="footer-widget__company-list list-unstyled">
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">How It Works</a></li>
-                                <li><a href="#">Knowledge hub</a></li>
-                                <li><a href="#">Success Stories</a></li>
-                                <li><a href="#">Contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-3 col-md-3">
-                        <div class="footer-widget__column footer-widget__explore wow fadeInUp animated"
-                            data-wow-delay="300ms"
-                            style="visibility: visible; animation-delay: 300ms; animation-name: fadeInUp;">
-                            <div class="footer-widget__title">
-                                <h3>Explore</h3>
+                            <div class="footer-widget_about_text">
+                                <a href="#">Naufal +62 822-4309-0750</a>
                             </div>
-                            <ul class="footer-widget__explore_list list-unstyled">
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">Trust & Safety</a></li>
-                                <li><a href="#">Help & Support</a></li>
-                                <li><a href="#">Press</a></li>
-                                <li><a href="#">Careers</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-4 col-md-4">
-                        <div class="footer-widget__column footer-widget__links wow fadeInUp animated"
-                            data-wow-delay="400ms"
-                            style="visibility: visible; animation-delay: 400ms; animation-name: fadeInUp;">
-                            <div class="footer-widget__title">
-                                <h3>Links</h3>
+                            <div class="footer-widget_about_text">
+                                <a href="#">Fio +62 812-7441-9210</a>
                             </div>
-                            <ul class="footer-widget_links_list list-unstyled">
-                                <li><a href="#">Privacy</a></li>
-                                <li><a href="#">Cookies</a></li>
-                                <li><a href="#">Funded Companies</a></li>
-                                <li><a href="#">Media Centre</a></li>
-                                <li><a href="#">Partnership</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-8 col-md-8">
-                        <div class="footer-widget__column footer-widget__options wow fadeInUp animated"
-                            data-wow-delay="400ms"
-                            style="visibility: visible; animation-delay: 400ms; animation-name: fadeInUp;">
-                            <div class="footer-widget__title">
-                                <h3>Options</h3>
+                            <div class="footer-widget_about_text">
+                                <a href="#">Mahez +62 896-3217-2873</a>
                             </div>
-                            <div class="footer_Currency">
-                                <select name="currency" id="currency" class="selectpicker">
-                                    <option value="Country">$ US Dollars </option>
-                                    <option value="Aud">AUD</option>
-                                    <option value="Aud">CAD</option>
-                                </select>
-                                <select name="language" id="language" class="selectpicker">
-                                    <option value="Country">English</option>
-                                    <option value="Canada">Russian</option>
-                                    <option value="England">Urdu</option>
-                                </select>
+                            <div class="footer-widget_about_text">
+                                <a href="#">Akbar +62 852-3158-5058</a>
+                            </div>
+                            <div class="footer-widget_about_text">
+                                <a href="#">Fadlan +62 853-2216-5023</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </footer>
+        
 
         <!--Site Footer Bottom Start-->
         <div class="site-footer_bottom">
@@ -101,13 +50,7 @@
                     <div class="site_footer_bottom_icon">
                         <img src={{asset("assets/images/shapes/footer-bottom-shape.png")}} alt="">
                     </div>
-                    <p>@ All copyright 2020, <a href="#">Layerdrops.com</a></p>
-                </div>
-                <div class="site-footer__social">
-                    <a href="#" class="tw-clr"><i class="fab fa-twitter"></i></a>
-                    <a href="#" class="fb-clr"><i class="fab fa-facebook-square"></i></a>
-                    <a href="#" class="dr-clr"><i class="fab fa-dribbble"></i></a>
-                    <a href="#" class="ins-clr"><i class="fab fa-instagram"></i></a>
+                    <p>@ All copyright 2023, <a href="#">FUNDPARTNER</a></p>
                 </div>
             </div>
         </div>
